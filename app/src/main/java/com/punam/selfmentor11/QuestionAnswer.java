97c7e6ead989c9bc26bd73840f2b4a9dd3aa6adc
Punam046 <kwrpunam046@gmail.com>
2023-03-02 03:45:18 -0800

    files added

---
+++ app/src/main/java/com/punam/selfmentor11/QuestionAnswer.java
@@ -0,0 +1,40 @@
+package com.punam.selfmentor11;
+
+public class QuestionAnswer {
+    public static String question[]={
+            "What is the first step in software development life cycle?",
+            "The __________ model helps in representing the system's dynamic behavior.",
+            "Which is identified as fourth generation language?",
+            "Which of the following falls under the category of software products?",
+            "Number of primitive data types in Java are?",
+            "Which of the following refers to internal software equality?",
+            "Which of the following is not an operating system?",
+            "When you delete a file in your computer, where does it go?",
+            "Carnotite is a mineral of which among the following metals?",
+            "Which of the following is controlled by the thyroid gland?"
+    };
+    public static String choices[][]={
+            {"System Design", "Coding", "System Testing", "Analysis"},
+            {"Object Model", "Context Model", "Behavioral Model", "Data Model"},
+            {"Unix shell", "C++", "COBOL", "FORTRAN"},
+            {"Firmware, CAD", "Embedded, CAM", "Customized, Generic", "CAD, Embedded"},
+            {"6", "7", "8", "9"},
+            {"Scalability", "Reusability", "Reliability", "Usability"},
+            {"Windows", "Linux", "Oracle", "DOS"},
+            {"Recycle bin", "Hard disk", "Taskbar", "c Drive"},
+            {"Lead", "Uranium", "Aluminium", "Iron"},
+            {"Glucose","motions","Metabolism","None"}
+    };
+    public static String correctAnswer[]={
+            "Analysis",
+            "Behavioral Model",
+            "Unix shell",
+            "Customized, Generic",
+            "8",
+            "Reusability",
+            "DOS",
+            "Recycle bin",
+            "Uranium",
+            "Metabolism"
+    };
+}
